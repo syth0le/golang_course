10156ee80369d33c75b16de71cc9e847d022d591
syth0le <chdan565@gmail.com>
2021-08-26 02:44:59 +0300

    task1: Read function done, start adding print functional

--- hw1_tree/main.go
+++ hw1_tree/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"path/filepath"
-
 	//"fmt"
 	"io"
 	"io/fs"
@@ -13,6 +12,30 @@ import (
 	//"strings"
 )
 
+type Node interface {
+	fmt.Stringer
+}
+
+type Directory struct{
+	name string
+	children []Node
+}
+
+type File struct{
+	name string
+	size int64
+}
+
+func (dir Directory) String() string {
+	return dir.name
+}
+
+func (file File) String() string {
+	if file.size != 0 {
+		return fmt.Sprintf("%s (%db)", file.name, file.size)
+	}
+	return fmt.Sprintf("%s (empty)", file.name)
+}
 
 func main() {
 	out := os.Stdout
@@ -29,25 +52,54 @@ func main() {
 }
 
 func dirTree(out io.Writer, path string, printFiles bool) error {
-	//println(out, path, printFiles)
-	dir, _ := ioutil.ReadDir(path)
-
-	for _, elem := range dir {
-
-		_ = toStringRecognizer(elem)
-
-		if elem.IsDir() {
-			levelPath := filepath.Join(path, elem.Name())
-			println("\n"+levelPath)
-
-			dirTree(out, levelPath, printFiles)
-		}
-	}
-
+	nodes := readDir(out , path, printFiles, []Node{})
+	printDir(out, nodes)
 	return nil
 }
 
-func toStringRecognizer(elem fs.FileInfo) error {
+
+func readDir(out io.Writer, path string, printFiles bool, nodes []Node) []Node {
+	dir, _ := ioutil.ReadDir(path)
+	var newNode Node
+
+	for _, elem := range dir {
+
+		//_ = toStringRecreator(elem)
+
+		switch elem.IsDir(){
+		case true:
+			levelPath := filepath.Join(path, elem.Name())
+			//println("\n"+levelPath)
+			children := readDir(out, levelPath, printFiles, []Node{})
+			newNode = Directory{elem.Name(), children}
+		case false:
+			newNode = File{elem.Name(), elem.Size()}
+
+		}
+
+		nodes = append(nodes, newNode)
+
+	}
+	return nodes
+}
+
+func printDir(out io.Writer, nodes []Node){
+	if len(nodes) == 1 {
+		fmt.Fprintf(out, "%s%s\n", "└───", nodes[0])
+		if directory, ok := nodes[0].(Directory); ok {
+			printDir(out, directory.children)
+		}
+		return
+	}
+	fmt.Fprintf(out, "%s%s\n", "├───", nodes[0])
+	if directory, ok := nodes[0].(Directory); ok {
+		printDir(out, directory.children)
+	}
+	printDir(out, nodes[1:])
+}
+
+
+func toStringRecreator (elem fs.FileInfo) error {
 	if elem == nil{
 		panic("can't get elements from slice")
 	}
@@ -55,13 +107,4 @@ func toStringRecognizer(elem fs.FileInfo) error {
 	println(toReturn)
 
 	return nil
-}
-
-
-func readDir(){
-
-}
-
-func printDir(){
-
 }
